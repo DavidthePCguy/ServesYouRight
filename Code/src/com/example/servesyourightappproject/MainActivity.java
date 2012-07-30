cad667d4a8c8714cd905996230fa25a27893cb52
David Kahn <sirdavidkahn1@gmail.com>
2012-07-30 13:39:47 -0400

    Tabs working!
    Make sure android.r; is deleted!

--- Code/src/com/example/servesyourightappproject/MainActivity.java
+++ Code/src/com/example/servesyourightappproject/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.servesyourightappproject;
 
+
+import android.R.layout;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.res.Resources;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 import android.support.v4.app.NavUtils;
 
 public class MainActivity extends Activity {
@@ -12,9 +18,36 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        
+        TabHost th = (TabHost) findViewById(R.id.tabhost);
+        th.setup();
+       // getWindow().setBackgroundDrawableResource(R.drawable.wall);
+        Button button1 = (Button) findViewById(R.id.button1);
+       // button1.setOnClickListener(this);
+        Resources res = getResources();
+        
+        //
+        TabSpec specs = th.newTabSpec("tag1");
+        specs.setContent(R.id.tab1);
+        specs.setIndicator("tab1"); //tabname
+        th.addTab(specs);
+       //
+        TabSpec specs1 = th.newTabSpec("tag2");
+        specs1.setContent(R.id.tab2);
+        specs1.setIndicator(""); //tabname
+        th.addTab(specs1);
+       //
+        TabSpec specs11 = th.newTabSpec("tag3");
+        specs11.setContent(R.id.tab3);
+        specs11.setIndicator(""); //tabname
+        th.addTab(specs11);
+       //
+    
+        
     }
 
-    @Override
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
