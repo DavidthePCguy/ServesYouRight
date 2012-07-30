41044c4dbc353c9fd8e9c1dda04ecfb891d77f7f
David Kahn <sirdavidkahn1@gmail.com>
2012-07-30 17:44:03 -0400

    My Promise to you
    (right before i add icon tabs)

--- Code/gen/com/example/servesyourightappproject/R.java
+++ Code/gen/com/example/servesyourightappproject/R.java
@@ -16,23 +16,25 @@ public final class R {
         public static final int padding_small=0x7f040000;
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int meetsusan=0x7f020002;
-        public static final int paper=0x7f020003;
+        public static final int about=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int meetsusan=0x7f020003;
+        public static final int paper=0x7f020004;
     }
     public static final class id {
+        public static final int ImageView01=0x7f080006;
         public static final int imageView1=0x7f080004;
-        public static final int menu_settings=0x7f08000a;
+        public static final int menu_settings=0x7f08000b;
         public static final int scrollView1=0x7f080002;
         public static final int tab1=0x7f080001;
-        public static final int tab2=0x7f080007;
-        public static final int tab3=0x7f080009;
+        public static final int tab2=0x7f080008;
+        public static final int tab3=0x7f08000a;
         public static final int tabhost=0x7f080000;
         public static final int tableRow1=0x7f080003;
         public static final int tableRow3=0x7f080005;
-        public static final int tableRow4=0x7f080006;
-        public static final int textView1=0x7f080008;
+        public static final int tableRow4=0x7f080007;
+        public static final int textView1=0x7f080009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
